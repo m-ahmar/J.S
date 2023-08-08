4cb993825ef22b726daf03457ffff17d885ad4df
m-ahmar <141359085+m-ahmar@users.noreply.github.com>
2023-08-08 05:49:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+// 1)Write a program to take input remaining fuel in the car (in liters) from the user. If the fuel is less than 0.25, display "Please refill the fuel in your car."
+
+// 2)Run this script and check whether alert messages would be displayed or not (various conditions provided).
+
+// 3)Write a program to take input marks obtained in three subjects and the total marks. Calculate the percentage and display the grade and remarks based on the percentage obtained.
+
+// 4)Initialize arrays with various data types (literalArr, stringArr, numberArr, boolArr, mixedArr, qualificationArr).
+
+// 5)Write a program to store phone manufacturers in an array.
+
+// 6)Write a program to store student scores in an array and sort the array in ascending order using Array's sort method.
+
+// 7)Write a program to create a single string from the given array ["This", "is", "my", "cat"].
+
+// 8)Implement the FIFO (First In First Out) approach using arrays.
+
+// 9)// Implement the LIFO (Last In First Out) approach using arrays.
+
+// 10)Write a program to enable "search by user input" in an array and prompt the user whether the item is found in the list or not.
+
+// 11)Write a program to identify the largest number in the given array.
+
+// 12)Write a program to identify the smallest number in the given array.
+
+// 13)Write a program to print multiples of 5 ranging from 1 to 100.
+// Write a program that takes two numbers & adds them to a new variable. Show the result in your browser.
+
+// 14)Repeat task1 for subtraction, multiplication, division & modulus.
+
+//15) Write a program to take "city" name as input from the user. If the user enters "Karachi", welcome the user like this: "Welcome to the city of lights."
+
+// 16)Write a program to take "gender" as input from the user. If the user is "Male," display "Good Morning Sir." If the user is "Female," display "Good Morning Ma'am."
+
+//17) Write a program to take input color of the road traffic signal. If the color is "Red," display "Must Stop." If the color is "Yellow," display "Ready to move." If the color is "Green," display "Move now."
